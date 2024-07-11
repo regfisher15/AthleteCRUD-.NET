3c19b50560535358a26a77dd256f6cfbbf3f955a
rbfisher15 <rbfisher@crimson.ua.edu>
2024-07-11 11:49:30 -0500

    Setup the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Athletes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
