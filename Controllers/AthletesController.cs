175a344e4c124750e498071abaff2a3f93959443
rbfisher15 <rbfisher@crimson.ua.edu>
2024-07-12 01:36:59 -0500

    Add create option for the table, i will fix image option later

--- Controllers/AthletesController.cs
+++ Controllers/AthletesController.cs
@@ -1,19 +1,71 @@
-﻿using Athletes.Services;
+﻿using Athletes.Models;
+using Athletes.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 
 namespace Athletes.Controllers
 {
     public class AthletesController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
 
-        public AthletesController(ApplicationDbContext context) {
+        public AthletesController(ApplicationDbContext context, IWebHostEnvironment environment) {
             this.context = context;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
-            var athletes = context.Athletes.ToList();
+            var athletes = context.Athletes.OrderByDescending(p => p.Id).ToList();
             return View(athletes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AthleteDto athleteDto)
+        {
+            if (athleteDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(athleteDto);
+            }
+
+            // save the image file
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(athleteDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/images/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                athleteDto.ImageFile.CopyTo(stream);
+            }
+
+            // save the new athelte in the database
+            Athlete athlete = new Athlete()
+            {
+                Name = athleteDto.Name,
+                PointsPerGame = athleteDto.PointsPerGame,
+                ReboundsPerGame = athleteDto.ReboundsPerGame,
+                AssistsPerGame = athleteDto.AssistsPerGame,
+                FieldGoalPercentage = athleteDto.FieldGoalPercentage,
+                ThreePointPercentage = athleteDto.ThreePointPercentage,
+                Championships = athleteDto.Championships,
+                ImageFileName = imageFullPath,
+                CreatedAt = DateTime.Now
+            };
+
+            context.Athletes.Add(athlete);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Athletes");
         }
     }
 }
---
+++ Models/AthleteDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Athletes.Models
+{
+    public class AthleteDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required]
+        public decimal PointsPerGame { get; set; }
+        [Required]
+        public decimal ReboundsPerGame { get; set; }
+        [Required]
+        public decimal AssistsPerGame { get; set; }
+        [Required]
+        public decimal FieldGoalPercentage { get; set; }
+        [Required]
+        public decimal ThreePointPercentage { get; set; }
+        [Required]
+        public int Championships { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
